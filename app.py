4188da39f341cb2ccd0340d31d33849ebdacea68
li jieeeee <153187094+LEE-CHIEN-AN@users.noreply.github.com>
2025-05-19 17:00:03 +0800

    Update app.py

--- app.py
+++ app.py
@@ -26,12 +26,7 @@ st.set_page_config(page_title="YouBike Station Dashboard", layout="wide")
 st.title("🚲 NTU History YouBike Station Dashboard 台大 YouBike 歷史紀錄 車站儀表板  24/09/01-24/12/25")
 
 # Page selector
-page = st.sidebar.radio("Choose a view:", [
-    "Map View 地圖",
-    "Current vs Stats 目前的 vs 統計資料",
-    "Hourly Line Chart 每小時折線圖",
-    "See-Bike Rate 分級"
-])
+page = st.sidebar.radio("Choose a view:", ["Map View 地圖","Current vs Stats 目前的 vs 統計資料", "Hourly Line Chart 每小時折線圖", "See-Bike Rate 分級"])
 
 if page == "Map View 地圖":
     st.header("🗺️ Station Map with Hourly Stats")
